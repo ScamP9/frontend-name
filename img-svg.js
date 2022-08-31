f8a25352926e24b73f741d59e5b750f9bf59393c
ScamP9 <98893443+ScamP9@users.noreply.github.com>
2022-08-31 23:42:01 +0400

    update 31.08.2022

--- img-svg.js
+++ img-svg.js
@@ -1,23 +1,19 @@
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 170) {
-        if ($('#Head').hover(function () {
-                $('.whitemenuuser').addClass('white_scroll');
-            },
-            function () {
-                if ($('.whitemenuuser').hover(function () {
-                    $('.whitemenuuser').removeClass('white_scroll');
-                }));
-            }));
+$(document).ready(function() {
+    $('#Head').hover(
+        function() { $(".white_field").css('display', 'block');},
+        function() { $('.white_field').css('display', 'none'); }
+    );
+});
+function SEO_text() {
+    let text = document.getElementById("Hidden");
+    let button = document.getElementById('buttonSEO')
+    if(text.style.display === "block") {
+        text.style.display = "none";
+        button.style.transform = "rotate(0deg)"
     }
     else {
-        $('.whitemenuuser').removeClass('white_scroll');
+        text.style.display = "block";
+        button.style.transform = "rotate(180deg)"
     }
-    if ($('.white_field').hover(function () {
-            $('.whitemenuuser').addClass('white_scroll');
-        },
-        function () {
-            if ($('.whitemenuuser').hover(function () {
-                $('.whitemenuuser').removeClass('white_scroll');
-            }));
-        }));
-});
+}
+
